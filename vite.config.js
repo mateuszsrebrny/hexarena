ca72f572d7e4d4226d58c4bb47cb3b12774cab1f
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-04 14:05:29 +0200

    Config demo-cube

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-export default {
-  server: {
-    open: ['/index.html', '/demo-cube.html'],
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'demo-cube/index.html'),
+      },
+    },
   },
-}
+})
