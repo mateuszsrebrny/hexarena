c751a2a38ee13c2fdf1afc518e8090eed2223c38
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-05 01:08:32 +0200

    Camera 45...

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 scene.background = THREE.Color("#0000CC");
 
 const camera = new THREE.PerspectiveCamera(
-  100,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
