abd4c1c7de7bc07108679f8f6ebe777244d82366
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-05 02:26:29 +0200

    envmap from texture

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 (async function() {
   let pmrem = new THREE.PMREMGenerator(renderer);
   let envmapTexture = await new RGBELoader().setDataType(THREE.FloatType).loadAsync("envmap.hdr");
-  
+  envmap = pmrem.fromEquirectangular(envmapTexture).texture;
   
   
   renderer.setAnimationLoop(() => {
