5ae589fcfd47d58f4605a1649b3b183442360671
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-05 22:10:21 -0700

    camera

--- main.js
+++ main.js
@@ -32,7 +32,8 @@ const camera = new PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(0, 0, 50);
+camera.position.set(-17, 31, 33);
+//camera.position.set(0, 0, 50);
 
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
