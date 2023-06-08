2b907ad57ac7c9961ee850d95cb91d20b2c80949
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-08 02:33:25 -0700

    light doesnt work, so mapintensity

--- main.js
+++ main.js
@@ -153,7 +153,8 @@ function hexMesh(geo, map) {
 
   let mat = new MeshPhysicalMaterial({
     envMap: envmap,
-    envMapIntensity: 0.135,
+    //envMapIntensity: 0.135,
+    envMapIntensity: 1,
     flatShading: true,
     map
   });
