a24957eb9a5148fe76f81ee93978ec5b9d2c2ff4
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-08 01:35:29 -0700

    shadow

--- main.js
+++ main.js
@@ -49,6 +49,12 @@ document.body.appendChild(renderer.domElement);
 
 const light = new PointLight(new Color("#FFCB8E").convertSRGBToLinear(), 80, 200);
 light.position.set(10, 20, 10);
+light.castShadow = true;
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
+scene.add(light);
 
 let envmap;
 
