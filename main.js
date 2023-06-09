6fc9b5ac9b960338dd1be0cc46b844d16cae047c
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-09 02:49:01 -0700

    tres

--- main.js
+++ main.js
@@ -245,3 +245,25 @@ function addStonesRandomly(height, position) {
     }
   }
 }
+
+function tree(height, position) {
+  const treeHeight = Math.random() * 1 + 1.25;
+
+  const geo1 = new CylinderGeometry(0, 1.5, treeHeight, 3);
+  geo1.translate(position.x, height + treeHeight * 0 + 1, position.y);
+
+  const geo2 = new CylinderGeometry(0, 1.15, treeHeight, 3);
+  geo2.translate(position.x, height + treeHeight * 0.6 + 1, position.y);
+
+  const geo3 = new CylinderGeometry(0, 0.8, treeHeight, 3);
+  geo3.translate(position.x, height + treeHeight * 1.25 + 1, position.y);
+
+  return mergeBufferGeometries([geo1, geo2, geo3]);
+
+}
+
+function addTreeRandomly(height, position) {
+  if (Math.random() > 0.8) {
+    grassGeo = mergeBufferGeometries([grassGeo, tree(height, position)]);
+  }
+}
