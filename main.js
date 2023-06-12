2375ed27df23f1bdfa9fc69b33612a911504df78
Mateusz Srebrny <mateusz.srebrny@gmail.com>
2023-06-12 14:55:16 -0700

    size 2

--- main.js
+++ main.js
@@ -334,6 +334,8 @@ function createMap() {
     [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
     [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
   ];
+
+  MAP_SIZE = 2;
   
   for (let i = -1*MAP_SIZE; i <= MAP_SIZE; ++i) {
     for (let j = -1*MAP_SIZE; j <= MAP_SIZE; ++j) {
@@ -341,7 +343,7 @@ function createMap() {
 
       if (position.length() > MAP_SIZE * 1.77 + 0.5) continue;
       
-      let height = mapHeights3[i + MAP_SIZE][j + MAP_SIZE];
+      let height = mapHeights2[i + MAP_SIZE][j + MAP_SIZE];
 
       makeHex(height * MAX_HEIGHT, position);
     }
